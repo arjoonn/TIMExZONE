3c4bf37ed84b7a2877074dce6937f741dcc7fe12
Arjun <arjuchempully@gmail.com>
2025-10-31 19:29:33 +0530

    fixed:API key brevo

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const { checkForAuthenticationCookie } = require("./middleware/authentication");
 
 app.use(
   cors({
-    origin:["https://tim-ex-zone.vercel.app",
+    origin:["tim-ex-zone.vercel.app",
     process.env.CLIENT_URI
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
